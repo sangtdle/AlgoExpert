4768a4701270afe90754dd6d4e22c2298b8294bc
sangtdle <sangtdle@kms-technology.com>
2023-10-06 13:28:32 +0700

    Solve the product sum problem

---
+++ AlgoExpert.Questions/RecursionQuestions/ProductSumQuestion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.RecursionQuestions
+{
+    public class ProductSumQuestion
+    {
+        // O(n) time || O(d) space - where n is the total number of elements in array,
+        // including sub-elements, and d is the greatest depth of "speical" array in array
+        public static int ProductSum1(List<object> array)
+        {
+            return ProductSum1Helper(array, 1);
+        }
+
+        private static int ProductSum1Helper(List<object> array, int multiplier)
+        {
+            int sum = 0;
+            foreach (object el in array)
+            {
+                if (el is IList<object>)
+                {
+                    sum += ProductSum1Helper((List<object>)el, multiplier + 1);
+                }
+                else
+                {
+                    sum += (int)el;
+                }
+            }
+
+
+            return sum * multiplier;
+        }
+    }
+}
---
+++ AlgoExpert.Tests/RecursionQuestionTests/ProductSumQuestionTest.cs
@@ -0,0 +1,32 @@
+﻿using AlgoExpert.Questions.RecursionQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.RecursionQuestionTests
+{
+    public class ProductSumQuestionTest
+    {
+        [Test]
+        public void TestProductSum1()
+        {
+            List<object> test = new List<object>() {
+              5,
+              2,
+              new List<object>() { 7, -1 },
+              3,
+              new List<object>() {
+                6,
+                new List<object>() { -13, 8 },
+                4,
+              },
+            };
+            var actualResult = ProductSumQuestion.ProductSum1(test);
+
+            Assert.IsTrue(actualResult == 12);
+        }
+    }
+}
