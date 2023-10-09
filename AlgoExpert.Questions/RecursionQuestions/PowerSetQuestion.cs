514d92ca24e49be9e313cfc81f19506516698c4f
sangtdle <sangtdle@kms-technology.com>
2023-10-09 22:40:30 +0700

    Solve Powerset problem

---
+++ AlgoExpert.Questions/RecursionQuestions/PowerSetQuestion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.RecursionQuestions
+{
+    public class PowerSetQuestion
+    {
+        public static List<List<int>> Powerset1(List<int> array)
+        {
+            List<List<int>> subsets = new List<List<int>>();
+            subsets.Add(new List<int>());
+
+            foreach (int ele in array)
+            {
+                int length = subsets.Count;
+                for (int i = 0; i < length; i++)
+                {
+                    List<int> currentSubset = new List<int>(subsets[i]);
+                    currentSubset.Add(ele);
+                    subsets.Add(currentSubset);
+                }
+            }
+
+            return subsets;
+        }
+
+        public static List<List<int>> Powerset2(List<int> array)
+        {
+            return PowerSet2Helper(array, array.Count - 1);
+        }
+
+        private static List<List<int>> PowerSet2Helper(List<int> array, int idx)
+        {
+            if (idx < 0)
+            {
+                List<List<int>> emptyset = new List<List<int>>();
+                emptyset.Add(new List<int>());
+                return emptyset;
+            }
+
+            int ele = array[idx];
+            List<List<int>> subsets = PowerSet2Helper(array, idx - 1);
+            int length = subsets.Count;
+            for (int i = 0; i < length; i++)
+            {
+                List<int> currentSubset = new List<int>(subsets[i]);
+                currentSubset.Add(ele);
+                subsets.Add(currentSubset);
+            }
+
+            return subsets;
+        }
+    }
+}
---
+++ AlgoExpert.Tests/RecursionQuestionTests/PowerSetQuestionTest.cs
@@ -0,0 +1,72 @@
+﻿using AlgoExpert.Questions.RecursionQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.RecursionQuestionTests
+{
+    public class PowerSetQuestionTest
+    {
+        [Test]
+        public void TestPowerSetQuestionTest1()
+        {
+            List<int> input = new List<int>() { 1, 2, 3 };
+            List<List<int>> subsets = PowerSetQuestion.Powerset1(input);
+
+            Assert.IsTrue(subsets.Count == 8);
+            Assert.IsTrue(Contains(subsets, new int[] { }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 2 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 2 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 3 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void TestPowerSetQuestionTest2()
+        {
+            List<int> input = new List<int>() { 1, 2, 3 };
+            List<List<int>> subsets = PowerSetQuestion.Powerset2(input);
+
+            Assert.IsTrue(subsets.Count == 8);
+            Assert.IsTrue(Contains(subsets, new int[] { }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 2 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 2 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 3 }));
+            Assert.IsTrue(Contains(subsets, new int[] { 1, 2, 3 }));
+        }
+
+        public bool Contains(List<List<int>> arr1, int[] arr2)
+        {
+            foreach (List<int> subArr in arr1)
+            {
+                subArr.Sort();
+                if (compare(subArr, arr2))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool compare(List<int> arr1, int[] arr2)
+        {
+            if (arr1.Count != arr2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < arr1.Count; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
