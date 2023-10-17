ca2124b3a2b2ee229d213b9c60b2d7578b779b59
sangtdle <sangtdle@kms-technology.com>
2023-10-17 11:58:14 +0700

    Solve phone number mnemorics problem

---
+++ AlgoExpert.Questions/RecursionQuestions/PhoneNumberMnemonicsQuestion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.RecursionQuestions
+{
+    public class PhoneNumberMnemonicsQuestion
+    {
+        public static Dictionary<char, string[]> DIGIT_LETTERS = new Dictionary<char, string[]>
+        {
+            { '1', new string[] { "1" } },
+            { '2', new string[] { "a", "b", "c" } },
+            { '3', new string[] { "d", "e", "f" } },
+            { '4', new string[] { "g", "h", "i" } },
+            { '5', new string[] { "j", "k", "l" } },
+            { '6', new string[] { "m", "n", "o" } },
+            { '7', new string[] { "p", "q", "r", "s" } },
+            { '8', new string[] { "t", "u", "v" } },
+            { '9', new string[] { "w", "x", "y", "z" } },
+            { '0', new string[] { "0" } },
+        };
+
+        public static List<string> PhoneNumberMnemonics1(string phoneNumber)
+        {
+            string[] currentMnemonic = new string[phoneNumber.Length];
+            Array.Fill(currentMnemonic, "0");
+
+            List<string> mnemonicsFound = new List<string>();
+            PhoneNumberMnemonics1Helper(0, phoneNumber, currentMnemonic, mnemonicsFound);
+            return mnemonicsFound;
+        }
+
+        private static void PhoneNumberMnemonics1Helper(int idx, string phoneNumber, string[] currentMnemonic, List<string> mnemonicsFound)
+        {
+            if (idx == phoneNumber.Length)
+            {
+                string mnemonic = string.Join("", currentMnemonic);
+                mnemonicsFound.Add(mnemonic);
+            }
+            else
+            {
+                char digit = phoneNumber[idx];
+                string[] letters = DIGIT_LETTERS[digit];
+                foreach (string letter in letters)
+                {
+                    currentMnemonic[idx] = letter;
+                    PhoneNumberMnemonics1Helper(idx + 1, phoneNumber, currentMnemonic, mnemonicsFound);
+                }
+            }
+        }
+    }
+}
---
+++ AlgoExpert.Tests/RecursionQuestionTests/PhoneNumberMnemonicsQuestionTest.cs
@@ -0,0 +1,40 @@
+﻿using AlgoExpert.Questions.RecursionQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.RecursionQuestionTests
+{
+    public class PhoneNumberMnemonicsQuestionTest
+    {
+        [Test]
+        public void TestPhoneNumberMnemonics1()
+        {
+            string phoneNumber = "1905";
+            string[] expectedValues = new string[] {
+              "1w0j",
+              "1w0k",
+              "1w0l",
+              "1x0j",
+              "1x0k",
+              "1x0l",
+              "1y0j",
+              "1y0k",
+              "1y0l",
+              "1z0j",
+              "1z0k",
+              "1z0l"
+            };
+            List<string> expected = new List<string>();
+            for (int i = 0; i < expectedValues.Length; i++)
+            {
+                expected.Add(expectedValues[i]);
+            }
+            var actual = PhoneNumberMnemonicsQuestion.PhoneNumberMnemonics1(phoneNumber);
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, actual));
+        }
+    }
+}
