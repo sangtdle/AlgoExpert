e6a54ad30b7ea7e67b20e4bab2d32fa671dd0bb9
sangtdle <sangtdle@kms-technology.com>
2023-10-20 12:00:39 +0700

    Solve backjack probability problem

---
+++ AlgoExpert.Questions/RecursionQuestions/BlackjackProbabilityQuestion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.RecursionQuestions
+{
+    public class BlackjackProbabilityQuestion
+    {
+        // O (t - s) time | O (t - s) space where t is the target and s is the starting hand
+        public static double BlackjackProbability1(int target, int startingHand)
+        {
+            Dictionary<int, double> memo = new Dictionary<int, double>();
+
+            return Math.Round(BlackjackProbability1Helper(startingHand, target, memo) * 1000f)/ 1000f;
+        }
+
+        private static double BlackjackProbability1Helper(int currentHand, int target, Dictionary<int, double> memo)
+        {
+            if (memo.ContainsKey(currentHand))
+            {
+                return memo[currentHand];
+            }
+
+            if (currentHand > target)
+            {
+                return 1;
+            }
+            if (currentHand >= target - 4)
+            {
+                return 0;
+            }
+
+            double totalProbability = 0;
+            for (int drawnCard = 1; drawnCard <= 10; drawnCard++)
+            {
+                totalProbability += .1 * BlackjackProbability1Helper(currentHand + drawnCard, target, memo);
+            }
+
+            memo[currentHand] = totalProbability;
+            return totalProbability;
+        }
+    }
+}
---
+++ AlgoExpert.Tests/RecursionQuestionTests/BackjackProbabilityQuestionTest.cs
@@ -0,0 +1,23 @@
+﻿using AlgoExpert.Questions.RecursionQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.RecursionQuestionTests
+{
+    public class BackjackProbabilityQuestionTest
+    {
+        [Test]
+        public void TestBackjackProbability1()
+        {
+            int target = 21;
+            int startingHand = 15;
+            double expected = 0.45;
+            double actual = BlackjackProbabilityQuestion.BlackjackProbability1(target, startingHand);
+            Assert.IsTrue(expected == actual);
+        }
+    }
+}
