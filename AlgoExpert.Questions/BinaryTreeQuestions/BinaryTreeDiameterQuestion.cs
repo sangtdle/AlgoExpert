17738146fd89266b01eab374750d01114b4a6119
sangtdle <sangtdle@kms-technology.com>
2024-03-24 16:54:11 +0700

    Solve Binary Tree Diameter problem

---
+++ AlgoExpert.Questions/BinaryTreeQuestions/BinaryTreeDiameterQuestion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.BinaryTreeQuestions
+{
+    public class BinaryTreeDiameterQuestion
+    {
+        public static int BinaryTreeDiameter(BinaryTree tree)
+        {
+            return getTreeInfo(tree).diameter;
+        }
+
+        private static TreeInfo getTreeInfo(BinaryTree tree)
+        {
+            if (tree == null) return new TreeInfo(0, 0);
+
+            TreeInfo leftTreeInfo = getTreeInfo(tree.left);
+            TreeInfo rightTreeInfo = getTreeInfo(tree.right);
+
+            int longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height;
+            int maxDiameterSoFar = Math.Max(leftTreeInfo.diameter, rightTreeInfo.diameter);
+
+            int currentDiameter = Math.Max(longestPathThroughRoot, maxDiameterSoFar);
+            int currentHeight = Math.Max(leftTreeInfo.height, rightTreeInfo.height) + 1;
+
+            return new TreeInfo(currentDiameter, currentHeight);
+        }
+
+        class TreeInfo
+        {
+            public int diameter;
+            public int height;
+
+            public TreeInfo(int diameter, int height)
+            {
+                this.diameter = diameter;
+                this.height = height;
+            }
+        }
+    }
+}
---
+++ AlgoExpert.Tests/BinaryTreeQuestionTests/BinaryTreeDiameterQuestionTest.cs
@@ -0,0 +1,29 @@
+﻿using AlgoExpert.Questions.BinaryTreeQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.BinaryTreeQuestionTests
+{
+    public class BinaryTreeDiameterQuestionTest
+    {
+        public void TestCase1()
+        {
+            var root = new BinaryTree(1);
+            root.left = new BinaryTree(3);
+            root.left.left = new BinaryTree(7);
+            root.left.left.left = new BinaryTree(8);
+            root.left.left.left.left = new BinaryTree(9);
+            root.left.right = new BinaryTree(4);
+            root.left.right.right = new BinaryTree(5);
+            root.left.right.right.right = new BinaryTree(6);
+            root.right = new BinaryTree(2);
+            var expected = 6;
+            var actual = BinaryTreeDiameterQuestion.BinaryTreeDiameter(root);
+            Assert.True(expected == actual);
+        }
+    }
+}
