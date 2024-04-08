fb9fe4a147ed69a0763ee34a92f3602c02b576c1
sangtdle <sangtdle@kms-technology.com>
2024-04-08 11:28:23 +0700

    Solve Symmetrical Tree problem

---
+++ AlgoExpert.Questions/BinaryTreeQuestions/SymmetricalTreeQuestion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Questions.BinaryTreeQuestions
+{
+    public class SymmetricalTreeQuestion
+    {
+        public static bool SymmetricalTree1(BinaryTree tree)
+        {
+            Stack<BinaryTree> leftStack = new Stack<BinaryTree>();
+            Stack<BinaryTree> rightStack = new Stack<BinaryTree>();
+
+            leftStack.Push(tree.left);
+            rightStack.Push(tree.right);
+
+            while (leftStack.Count > 0 && rightStack.Count > 0)
+            {
+                BinaryTree leftNode = leftStack.Pop();
+                BinaryTree rightNode = rightStack.Pop();
+
+                if (leftNode == null && rightNode == null)
+                {
+                    continue;
+                }
+
+                if (leftNode != null && rightNode != null && leftNode.value != rightNode.value)
+                {
+                    return false;
+                }
+
+                leftStack.Push(leftNode.left);
+                leftStack.Push(leftNode.right);
+
+                rightStack.Push(rightNode.right);
+                rightStack.Push(rightNode.left);
+
+            }
+
+            return leftStack.Count == 0 && rightStack.Count == 0;
+        }
+
+        public static bool SymmetricalTree2(BinaryTree tree)
+        {
+            return SymmetricalTreeHelper(tree.left, tree.right);
+        }
+
+        private static bool SymmetricalTreeHelper(BinaryTree left, BinaryTree right)
+        {
+            if (left != null && right != null)
+            {
+                return left.value == right.value && SymmetricalTreeHelper(left.left, right.right) && SymmetricalTreeHelper(left.right, right.left);
+            }
+
+            return left == right;
+        }
+    }
+}
---
+++ AlgoExpert.Tests/BinaryTreeQuestionTests/SymmetricalTreeQuestionTest.cs
@@ -0,0 +1,43 @@
+﻿using AlgoExpert.Questions.BinaryTreeQuestions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Tests.BinaryTreeQuestionTests
+{
+    public class SymmetricalTreeQuestionTest
+    {
+        [Fact]
+        public void TestCase1()
+        {
+            BinaryTree tree = new BinaryTree(10);
+            tree.left = new BinaryTree(5);
+            tree.right = new BinaryTree(5);
+            tree.left.left = new BinaryTree(7);
+            tree.left.right = new BinaryTree(9);
+            tree.right.left = new BinaryTree(9);
+            tree.right.right = new BinaryTree(7);
+            var expected = true;
+            var actual = SymmetricalTreeQuestion.SymmetricalTree1(tree);
+            Assert.True(expected == actual);
+        }
+
+        [Fact]
+        public void TestCase2()
+        {
+            BinaryTree tree = new BinaryTree(10);
+            tree.left = new BinaryTree(5);
+            tree.right = new BinaryTree(5);
+            tree.left.left = new BinaryTree(7);
+            tree.left.right = new BinaryTree(9);
+            tree.right.left = new BinaryTree(9);
+            tree.right.right = new BinaryTree(7);
+            var expected = true;
+            var actual = SymmetricalTreeQuestion.SymmetricalTree2(tree);
+            Assert.True(expected == actual);
+        }
+    }
+}
